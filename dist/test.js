7a4f136c64926d03ad659d5f070d75a4f8e6aa29
Mofassel100 <smmofasselhosain.bba.ac.22@gmail.com>
2026-05-02 22:44:08 +0600

    I am practicng basic type

---
+++ dist/test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Mofassel = "mofassel hosaing";
+//# sourceMappingURL=test.js.map
---
+++ src/test.ts
@@ -0,0 +1,15 @@
+const Mofassel: string = "mofassel hosaing";
+// console.log(Mofassel);
+const AreYouFine: boolean = false;
+// console.log(AreYouFine);
+function Mofasse(name: string, email: string): string {
+  const fullS = `Name: ${name}   Email: ${email}`;
+  return fullS;
+}
+// console.log(Mofasse("Mofassel Hosain", "mofassel@gmail.com"));
+
+const Mofssel = (Name: string, Email: string): string => {
+  const fullS = `Name: ${Name}   Email: ${Email}`;
+  return fullS;
+};
+console.log(Mofasse("Mofassel Hosain", "mofassel@gmail.com"));
